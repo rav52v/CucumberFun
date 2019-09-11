1f3e7dc7c66a6fc66211b74a9cb075f2f8bfaa88
RCzekalski <rav52v@gmail.com>
2019-09-11 17:04:07 +0200

    pom fix

--- src/test/runners/MainTestRunner.java
+++ src/test/runners/MainTestRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
         features = {"src/test/features"},
         glue = {"steps/", "java1/"},
         plugin = {"html:target/cukes", "json:target/cucumber-template-report.json", "junit:target/cucumber-template-report.xml", "pretty"},
-        tags = {"@test"}
+        tags = {"@test1"}
 )
 
 public class MainTestRunner {
